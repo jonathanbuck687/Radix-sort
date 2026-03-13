bf30480ffa334a538ba2c1b04667784fcec40c72
jonathanbuck687 <jonathanb138@lpsk12.org>
2026-03-13 10:52:31 -0600

    im done

--- tests/RadixSortTest.java
+++ tests/RadixSortTest.java
@@ -1,19 +1,25 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 class RadixSortTest {
 
-    @org.junit.jupiter.api.Test
-    void listToFile() {
-
-    }
-
     @Test
-    void main() {
-    }
+    void main() throws IOException {
 
-    @Test
-    void getNum() {
+        File t1key = new File("sort_key.txt");
+        Assertions.assertTrue(Files.mismatch(RadixSort.main("sort").toPath(), t1key.toPath()) == -1);
+
+
+        File t2key = new File("sort(1)_key.txt");
+        Assertions.assertTrue(Files.mismatch(RadixSort.main("sort(1)").toPath(), t2key.toPath()) == -1);
+
+
+        File t3key = new File("sort(2)_key.txt");
+        Assertions.assertTrue(Files.mismatch(RadixSort.main("sort(2)").toPath(), t3key.toPath()) == -1);
     }
 }
+
+
